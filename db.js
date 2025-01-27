1bf9cfb8615f1bd7298ae481aa2f1efc74d35e8c
Matvey <caplinmatvey.12345@gmail.com>
2025-01-28 01:18:47 +0500

    change bd creds and add .env lib

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 let db = null;
 
 try {
 
     db = new Pool({
-        connectionString: "postgres://fgtmqmug:lBjdCD8OI8EVon9ozLFQk0wTt1ux1amk@cornelius.db.elephantsql.com/fgtmqmug",
-        ssl: true
+        connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@141.8.192.164:5432/${process.env.DB_NAME}`
     });
 
 } catch(e) {
